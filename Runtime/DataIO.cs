03f31db05393be222dfb7cc4937ffd657cf73a43
IdkL0 <theidkl042@gmail.com>
2026-06-21 17:33:03 +0300

    Added DataIO.cs for saving to files fk.sdfvbskllsdfapl;sdajahfcsdiopjkasdsda;ls'd;s

---
+++ Runtime/DataIO.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+
+namespace IdkL0.SimpleDataSaver
+{
+    public static class DataIO
+    {
+        public static void WriteToStream(Stream stream, DataSave data)
+        {
+            if (!stream.CanWrite) return;
+
+            stream.Write(data.GetBytes());
+        }
+
+        public static FileStream CreateFileSave(string filename, DataSave data)
+        {
+            FileStream stream = File.Create(filename);
+
+            WriteToStream(stream, data);
+
+            stream.Close();
+
+            return stream;
+        }
+    }
+}
